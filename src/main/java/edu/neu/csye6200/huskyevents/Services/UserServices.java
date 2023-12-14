a5868bd5bdb3fe8a1bfa4ef58ce5cdcb202d4a45
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-14 15:25:28 -0500

    fixed user controllers and services

--- src/main/java/edu/neu/csye6200/huskyevents/Services/UserServices.java
+++ src/main/java/edu/neu/csye6200/huskyevents/Services/UserServices.java
@@ -31,6 +31,7 @@ public class UserServices {
         User user = userRepository.findById(userID).get();
         return user;
     }
+    
     public User updateUser(String userID, User user) {
         user.setId(userID);
         User userUpdated = userRepository.save(user);
