300d19e18dbb7da77cbe98bf0ce3bd99971e995b
Tejashree Gore <gore.te@northeastern.edu>
2023-12-14 12:54:33 -0500

    first commit

---
+++ src/main/java/edu/neu/csye6200/huskyevents/HuskyEventsApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200.huskyevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskyEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/neu/csye6200/huskyevents/HuskyEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200.huskyevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskyEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
